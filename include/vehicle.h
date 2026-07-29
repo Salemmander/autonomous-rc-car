2a63c5de949461d1c7d788b785f18f051d0d1bbc
Salem Nassar <salemnassar5@gmail.com>
2026-07-29 17:49:50 -0500

    vehicle class and test

--- include/vehicle.h
+++ include/vehicle.h
@@ -1,2 +1,36 @@
+#pragma once
 
-class Vehicle {};
+#include "steering.h"
+#include "throttle.h"
+
+class Vehicle {
+private:
+    SteeringController steeringctl;
+    ThrottleController throttlectl;
+
+    float current_steering_angle{0.0};
+    float current_throttle{0.0};
+
+public:
+    ~Vehicle() {
+        stop();
+    }
+
+    bool ok() const {
+        return steeringctl.ok() && throttlectl.ok();
+    }
+    void drive(float angle, float throttle) {
+        if (!ok()) {
+            return;
+        }
+        steeringctl.setSteeringAngle(angle);
+        throttlectl.setThrottle(throttle);
+
+        current_steering_angle = angle;
+        current_throttle = throttle;
+    }
+
+    void stop() {
+        drive(0.0, 0.0);
+    }
+};
