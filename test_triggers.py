2a880c6c4243d73d0a04477aae2d5886ee791037
Salem Nassar <salemnassar5@gmail.com>
2026-08-16 18:26:32 -0500

    delete old python code

--- test_triggers.py
+++
@@ -1,8 +0,0 @@
-import evdev
-
-dev = evdev.InputDevice("/dev/input/event5")
-print("Pull each trigger separately. Ctrl+C to stop.\n")
-
-for event in dev.read_loop():
-    if event.type == 3 and event.code != 0:
-        print(f"code={event.code} value={event.value}")
